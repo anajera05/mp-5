aa0870c962589b7f207e00dc234cf6e827efea64
alyssa najera <anajera@bu.edu>
2025-04-18 18:00:26 -0400

    fixing [location]/page.tsx

--- app/[location]/page.tsx
+++ app/[location]/page.tsx
@@ -1,7 +1,14 @@
 import { redirect, notFound } from "next/navigation";
 import getCollection, { URL_COLLECTION } from "@/db";
 
-export default async function RedirectPage({ params }: { params: { location: string } }) {
+// Define the params separately
+type PageProps = {
+  params: {
+    location: string;
+  };
+};
+
+export default async function RedirectPage({ params }: PageProps) {
   const collection = await getCollection(URL_COLLECTION);
   const result = await collection.findOne({ short: params.location });
 
