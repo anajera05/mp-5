c69ca86570ce7a10029346fac23d325a11ae94dd
alyssa najera <anajera@bu.edu>
2025-04-18 23:27:04 -0400

    fixed dynamic routing

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,16 +1,16 @@
 import { redirect, notFound } from "next/navigation";
 import getCollection, { URL_COLLECTION } from "@/db";
-import { NewURL } from "@/types";
 
 type PageProps = {
   params: {
-    location: string;
+    id: string;
   };
 };
 
 export default async function RedirectPage({ params }: PageProps) {
+  const short = params.id;
   const collection = await getCollection(URL_COLLECTION);
-  const result = await collection.findOne({ short: params.location });
+  const result = await collection.findOne({ short: short });
 
   if (!result) {
     notFound();
