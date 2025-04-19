e4b105a293f0534339b0eb37c97b506ef12cc98f
alyssa najera <anajera@bu.edu>
2025-04-19 00:03:57 -0400

    PLEASE WORK THIS TIME :(

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -7,9 +7,11 @@ export default async function RedirectPage(props: {
   params: Params
 }) {
   const { slug } = props.params;
+  console.log(slug)
 
   const collection = await getCollection(URL_COLLECTION);
   const result = await collection.findOne({ short: slug });
+  console.log(result)
 
   if (!result) {
     notFound();
