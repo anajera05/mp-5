51d2f16c0419af8f81778906efb4e184fb3ac1bb
alyssa najera <anajera@bu.edu>
2025-04-18 23:59:35 -0400

    fix error

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,17 +1,15 @@
-"use server";
-
 import { redirect, notFound } from "next/navigation";
 import getCollection, { URL_COLLECTION } from "@/db";
 
-type Params = Promise<{ slug: string }>
+type Params = { slug: string }
 
 export default async function RedirectPage(props: {
   params: Params
 }) {
-  const params = await props.params
+  const { slug } = props.params;
 
   const collection = await getCollection(URL_COLLECTION);
-  const result = await collection.findOne({ params });
+  const result = await collection.findOne({ short: slug });
 
   if (!result) {
     notFound();
